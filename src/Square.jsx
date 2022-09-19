cf4c0195e5b22ce38c06d56f14db8199df90a993
= <neymegen50@gmail.com>
2022-09-20 00:17:41 +0300

    message

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 function Square(props) {
     return (
-      <button className="square" onClick={props.onClick()}>{props.value}</button>
+      <button className="square" onClick={props.onClick}>{props.value}</button>
     );
   };
 
